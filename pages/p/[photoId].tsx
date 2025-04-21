4b1afa264b960b11a9aa03598ce47426096c0937
gigihdvlpr <gigih.dvlpr@gmail.com>
2025-04-21 11:26:23 +0700

    modify v2

--- pages/p/[photoId].tsx
+++ pages/p/[photoId].tsx
@@ -13,6 +13,8 @@ const Home: NextPage = ({ currentPhoto }: { currentPhoto: ImageProps }) => {
   let index = Number(photoId);
 
   const currentPhotoUrl = `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_2560/${currentPhoto.public_id}.${currentPhoto.format}`;
+  
+  console.log("[Client-side] Current photo URL:", currentPhotoUrl); 
 
   return (
     <>
@@ -32,6 +34,8 @@ export default Home;
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const results = await getResults();
+  
+  console.log("[getStaticProps] First item from Cloudinary:", results.resources[0]); 
 
   let reducedResults: ImageProps[] = [];
   let i = 0;
@@ -49,7 +53,17 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const currentPhoto = reducedResults.find(
     (img) => img.id === Number(context.params.photoId),
   );
+
+  if (!currentPhoto) {
+    return { notFound: true };
+  }
+
   currentPhoto.blurDataUrl = await getBase64ImageUrl(currentPhoto);
+
+  const generatedUrl = `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${currentPhoto.public_id}.${currentPhoto.format}`;
+  
+  console.log("[getStaticProps] Current photo data:", currentPhoto); 
+  console.log("[getStaticProps] Generated URL:", generatedUrl); 
 
   return {
     props: {
@@ -64,6 +78,8 @@ export async function getStaticPaths() {
     .sort_by("public_id", "desc")
     .max_results(400)
     .execute();
+
+  console.log("[getStaticPaths] Total resources:", results.resources.length);  Optional
 
   let fullPaths = [];
   for (let i = 0; i < results.resources.length; i++) {
