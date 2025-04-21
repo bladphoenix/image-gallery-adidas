ff6416593edea4a7bdc71fd74a7c2b4b538b8fde
gigihdvlpr <gigih.dvlpr@gmail.com>
2025-04-21 13:16:43 +0700

    edit next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
         protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
-        pathname: "/my-account/**",
+        pathname: "/dmis60dxy/**",
         search: "",
       },
     ],
